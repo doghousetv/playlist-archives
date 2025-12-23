cbbc409fffd199c3f130eef53ad4b0c466a38848
DOGHOUSETV <arubigeorge0@gmail.com>
2025-12-23 13:19:22 -0500

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["generated"],
+  serverExternalPackages: ["@prisma/client", "prisma"],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
