c012edc09ca449789e2dcf3598f2f465c45d7d85
DOGHOUSETV <arubigeorge0@gmail.com>
2025-12-23 21:02:33 -0500

    export playlist scraper service modules

---
+++ lib/services/playlist-scraper/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Playlist Scraper Service
+ * 
+ * Modular service for scraping playlist metadata from Spotify and Apple Music URLs.
+ * 
+ * @module lib/services/playlist-scraper
+ */
+
+export { scrapePlaylistMetadata } from "./scraper"
+export type { ScrapedPlaylistData } from "./types"
+
