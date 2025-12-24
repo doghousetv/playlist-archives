2c4551eb55198278a920e192d62f6dfd6838f5f3
DOGHOUSETV <arubigeorge0@gmail.com>
2025-12-23 21:34:15 -0500

    add playlist response type

--- types/playlist.ts
+++ types/playlist.ts
@@ -13,6 +13,17 @@ export interface Playlist {
   platform?: "spotify" | "apple-music"
 }
 
+export interface PlaylistResponse {
+  playlists: Playlist[]
+  pagination: {
+    page: number
+    limit: number
+    totalCount: number
+    totalPages: number
+    hasMore: boolean
+  }
+}
+
 export interface PlaylistCardProps {
   playlist: Playlist
   index: number
